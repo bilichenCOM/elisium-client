fd1da4346991b0d0662ea11802fe8cda3951a2c1
bilichenCOM <bilichenko.mykhailo@gmail.com>
2019-08-28 01:59:28 +0300

    Added contacts page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from './home/home.component';
 import { ItemsComponent } from './items/items.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 const routes: Routes = [
 	{ path: 'home', component: HomeComponent },
 	{ path: 'categories', component: CategoriesComponent },
 	{ path: 'items', component: ItemsComponent },
 	{ path: 'about-us', component: AboutUsComponent },
+	{ path: 'contacts', component: ContactsComponent },
 	{ path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
