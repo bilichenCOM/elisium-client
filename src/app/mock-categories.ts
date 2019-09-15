68fbc8311ae02669c1acde770e12692f310fb26d
bilichenCOM <bilichenko.mykhailo@gmail.com>
2019-09-15 11:18:55 +0300

    composed single html page for speed optimization, modal map image added, style reviewed

--- src/app/mock-categories.ts
+++ src/app/mock-categories.ts
@@ -3,9 +3,9 @@ import { ITEMS } from './mock-items';
 
 export const CATEGORIES: Category[] = [
 	{ id: 1, name: 'Маргарини', description: 'Для листкового і пісочного тіста, хлібобулочної промисловості, для кремів, маргарини столові.', imageUrl: '../assets/images/categories/1.png', items: [] },
-	{ id: 2, name: 'Какао Масла', description: 'Еквіваленти какао масла, нелауринові замінники какао масла CBR, лауринові замінники какао масла CBS', imageUrl: '../assets/images/categories/2.png', items: [] },
+	{ id: 2, name: 'Какао Масло', description: 'Еквіваленти какао масла, нелауринові замінники какао масла CBR, лауринові замінники какао масла CBS', imageUrl: '../assets/images/categories/2.png', items: [] },
 	{ id: 3, name: 'Замінники молочного жиру', description: 'Жири для молочної промисловості', imageUrl: '../assets/images/categories/3.png', items: [] },
-	{ id: 4, name: 'Тропічні масла', description: 'Гідрогенізована РДО пальмова олія, РДО пальмовий олеїн, РДО пальмовий стеарин, РДО кокосове масло', imageUrl: '../assets/images/categories/4.png', items: [] },
+	{ id: 4, name: 'Тропічне масло', description: 'Гідрогенізована РДО пальмова олія, РДО пальмовий олеїн, РДО пальмовий стеарин, РДО кокосове масло', imageUrl: '../assets/images/categories/4.png', items: [] },
 	{ id: 5, name: 'Спеціалізовані жири', description: 'Жир кондитерський, жир харчовий загального призначення, жир кулінарний фритюрний, шортенінг універсальний. Жири для шоколаду. Жир кондитерський для шоколадних виробів, цукерок «КР-1», «КР-2», «КР-3», жир рослинний нелауриновий замінник какао масла', imageUrl: '../assets/images/categories/5.png', items: [] },
 	{ id: 6, name: 'Соняшникова олія', description: '', imageUrl: '../assets/images/categories/6.png', items: [] }
 ]
